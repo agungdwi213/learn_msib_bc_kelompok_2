1142a0077ac0448f0fa56f81c1f9b622096d34e0
agungdwi213 <aprasetyo345@gmail.com>
2023-09-30 07:51:47 +0700

    PoW

--- Blokchain/workchain.js
+++ Blokchain/workchain.js
@@ -6,28 +6,42 @@ class Block{
         this.data = data;
         this.hash = this.getHash();
         this.prevHash ="";
+        this.nonce =0;
     }
 
     getHash(){
-        return SHA256(JSON.stringify(this.data) + this.timestamp + this.prevHash);
+        return SHA256(JSON.stringify(this.data) + this.timestamp + this.prevHash); 
+    }
+    mine(difficulty){
+        while(!this.hash.startsWith(Array(difficulty + 1).join("0"))){
+            this.nonce++;
+            this.hash = this.getHash();
+        }
     }
 }
 
-class Blokchain {
+class Blockchain {
     constructor(){
         this.chain = [new Block(Date.now().toString())];
+        this.difficulty = 1;
+        this.blockTime = 30000;
     }
     getLastBlock(){
         return this.chain[this.chain.length - 1];
     }
+
     addBlock(block){
         block.prevHash = this.getLastBlock().hash;
         block.hash = block.getHash();
+ 
+        block.mine(this.difficulty);
         
+        this.difficulty += Date.now() - parseInt(this.getLastBlock().timestamp) < this.blocktime ? 1 : -1;
+
         this.chain.push(block);
     }
     isValid(blockchain = this){
-        for(let i = 0; i < blockchain.chain.length; i++){
+        for(let i = 1; i < blockchain.chain.length; i++){
             const currentBlock = blockchain.chain[i];
             const prevBlock = blockchain.chain[i-1];
 
@@ -39,7 +53,9 @@ class Blokchain {
     }
 }
 
-const workChain = new Blokchain();
-workChain.addBlock(new Block(Date.now().toString(), ['Hello BUILDERS']));
-workChain.chain[1].data = [1];
-console.log(workChain.isValid());
+const workChain = new Blockchain();
+workChain.addBlock(new Block(Date.now().toString(), ['Hello BUILDERS1']));
+workChain.addBlock(new Block(Date.now().toString(), ['Hello BUILDERS2']));
+workChain.addBlock(new Block(Date.now().toString(), ['Hello BUILDERS3']));
+
+console.log(workChain);   
